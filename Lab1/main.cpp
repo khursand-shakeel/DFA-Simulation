5f30ab3c8d0262eb8bc4639b77c20254b7a8bc2e
Muhammad Shehroz Sajjad <14bscsmsajjad@seecs.edu.pk>
2017-09-19 14:50:33 +0500

    Initial Commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Shehroz Sajjad on 9/19/17.
+//  Copyright © 2017 Shehroz Sajjad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
